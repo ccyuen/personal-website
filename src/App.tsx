a9d21ced705f52e30887383c8597e0a31d8be9f8
Clement Yuen <ccyuen@uwaterloo.ca>
2020-08-06 16:15:52 -0400

    project restartu

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
-import React from "react";
-import "./App.css";
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-const App: React.FC = () => {
+function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -16,6 +21,6 @@ const App: React.FC = () => {
       </header>
     </div>
   );
-};
+}
 
 export default App;
